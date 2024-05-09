9242a9a22b1d13ed366bd7e41849c489c9fed052
Jack <60209373+OpoJack@users.noreply.github.com>
2024-05-09 08:32:50 -0500

    Add basic tools + config (#1)
    * Add tailwind
    
    * Add biome
    
    Ran a format on every file in the repo so we shouldn't have format changes in the future.
    Feel free to adjust format rules as you prefer, just run a `npm run format` before you commit.
    
    * Add prettier + config
    
    In vscode settings, you need to:
    1. Open one of the html files in the project. Ctrl+shift+p "Format document with" and set the default to prettier.
    
    2. In vscode settings, search for prettier. Make sure the config path points to .prettierrc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'budget-bill';
+  title = 'budget-bill'
 }
