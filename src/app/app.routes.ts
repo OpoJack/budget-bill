0f38ebd072370cf67a0022aad3883c310ada16bc
Conner Luzier <connerluzier@outlook.com>
2024-05-12 15:45:59 -0400

    adding routes to app.routes.ts, buttons on app.component.html for testing (#4)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import type { Routes } from '@angular/router'
+import { DashboardComponent } from './components/dashboard/dashboard.component'
+import { AuthComponent } from './components/auth/auth.component'
+import { SettingsComponent } from './components/settings/settings.component'
 
-export const routes: Routes = []
+export const routes: Routes = [
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'settings', component: SettingsComponent},
+    {path: 'auth', component: AuthComponent},
+]
